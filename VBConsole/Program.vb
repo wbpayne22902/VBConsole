20becbf4e0ee5d94b0a4fbb4e4468789373d0ca4
wbpayne <wbpayne@WILHELMVM>
2022-06-16 22:30:23 -0400

    Add project files.

---
+++ VBConsole/Program.vb
@@ -0,0 +1,10 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Console.WriteLine("Hello World!")
+        For x = 1 To 15
+            Console.WriteLine($"Hello {x}")
+        Next
+    End Sub
+End Module
