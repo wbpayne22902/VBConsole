14a19091584515761ab71d8b80021524381dbc49
Wilhelm Payne <wbpayne@wilhelmpayne.com>
2022-06-17 21:28:36 -0400

    cool changes

--- VBConsole/Program.vb
+++ VBConsole/Program.vb
@@ -2,9 +2,13 @@ Imports System
 
 Module Program
     Sub Main(args As String())
+        Console.Clear()
+        Console.BackgroundColor = ConsoleColor.Blue
         Console.WriteLine("Hello World!")
         For x = 1 To 15
             Console.WriteLine($"Hello {x}")
         Next
+        Console.BackgroundColor = ConsoleColor.Black
+        Console.WriteLine("Done!")
     End Sub
 End Module
