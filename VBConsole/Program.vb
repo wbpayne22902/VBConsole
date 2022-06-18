c093174c251af225e3aa84e8ea7076f7769d0529
Wilhelm Payne <wbpayne@wilhelmpayne.com>
2022-06-17 21:59:21 -0400

    Changes made

--- VBConsole/Program.vb
+++ VBConsole/Program.vb
@@ -4,7 +4,7 @@ Module Program
     Sub Main(args As String())
         Console.Clear()
         Console.BackgroundColor = ConsoleColor.Blue
-        Console.WriteLine("Hello World!")
+        Console.WriteLine("Hello World! {0}", args(0))
         For x = 1 To 15
             Console.WriteLine($"Hello {x}")
         Next
